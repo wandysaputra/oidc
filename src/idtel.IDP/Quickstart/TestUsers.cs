e4a6990c2ded681e6bea571b24b52bf96b3922c7
wandy.saputra@hotmail.com <wandys@bizcover.com.au>
2022-08-03 19:52:07 +0700

    re-formatted files

--- src/idtel.IDP/Quickstart/TestUsers.cs
+++ src/idtel.IDP/Quickstart/TestUsers.cs
@@ -1,51 +1,43 @@
 ﻿// Copyright (c) Brock Allen & Dominick Baier. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
 
-
-using IdentityModel;
-using IdentityServer4.Test;
 using System.Collections.Generic;
 using System.Security.Claims;
 using System.Text.Json;
+using IdentityModel;
 using IdentityServer4;
+using IdentityServer4.Test;
 
-namespace IdentityServerHost.Quickstart.UI
-{
-    public class TestUsers
-    {
-        public static List<TestUser> Users = new List<TestUser>            
-        {
-             new TestUser
-             {
-                 SubjectId = "d860efca-22d9-47fd-8249-791ba61b07c7",
-                 Username = "Frank",
-                 Password = "password",
+namespace IdentityServerHost.Quickstart.UI {
+    public class TestUsers {
+        public static List<TestUser> Users = new List<TestUser> {
+            new TestUser {
+            SubjectId = "d860efca-22d9-47fd-8249-791ba61b07c7",
+            Username = "Frank",
+            Password = "password",
 
-                 Claims = new List<Claim>
-                 {
-                     new Claim("given_name", "Frank"),
-                     new Claim("family_name", "Underwood"),
-                     new Claim("address", "Main Road 1"), // to be show in address resource/claim
-                     new Claim("role", "FreeUser"),
+            Claims = new List<Claim> {
+            new Claim ("given_name", "Frank"),
+            new Claim ("family_name", "Underwood"),
+            new Claim ("address", "Main Road 1"), // to be show in address resource/claim
+            new Claim ("role", "FreeUser"),
 
-                 }
-             },
-             new TestUser
-             {
-                 SubjectId = "b7539694-97e7-4dfe-84da-b4256e1ff5c7",
-                 Username = "Claire",
-                 Password = "password",
+            }
+            },
+            new TestUser {
+            SubjectId = "b7539694-97e7-4dfe-84da-b4256e1ff5c7",
+            Username = "Claire",
+            Password = "password",
 
-                 Claims = new List<Claim>
-                 {
-                     new Claim("given_name", "Claire"),
-                     new Claim("family_name", "Underwood"),
-                     new Claim("address", "Big Street 2"),
-                     new Claim("role", "PayingUser"),
-                 }
-             }
-         };
-         /*
+            Claims = new List<Claim> {
+            new Claim ("given_name", "Claire"),
+            new Claim ("family_name", "Underwood"),
+            new Claim ("address", "Big Street 2"),
+            new Claim ("role", "PayingUser"),
+            }
+            }
+        };
+        /*
         public static List<TestUser> Users
         {
             get
@@ -57,7 +49,7 @@ namespace IdentityServerHost.Quickstart.UI
                     postal_code = 69118,
                     country = "Germany"
                 };
-                
+
                 return new List<TestUser>
                 {
                     new TestUser
